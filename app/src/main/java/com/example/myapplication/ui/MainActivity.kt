02757e54b6f34853f63885ce2a72a71eac53cd55
Sumit Rai <developer2607@gmail.com>
2022-02-17 21:41:52 +0530

    done

--- app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -1,16 +1,21 @@
-package com.example.myapplication
+package com.example.myapplication.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.R
 import com.example.myapplication.viewmodel.ChatbotViewModel
 import com.example.myapplication.viewmodel.OrderStatusViewModel
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity :AppCompatActivity()
 {
+    var blink_anim: Animation? = null
     var viewModel: ChatbotViewModel?=null
     var oderStatusViewModel : OrderStatusViewModel? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,16 +26,28 @@ class MainActivity :AppCompatActivity()
         viewModel?.cancelOrderApi("10","1a9eddfe5e5b2c75f6c15e739e05ab46",100002)
         viewModel?.cancelOrderResponse?.observe(this) {
             Log.d("sddffsddsds", Gson().toJson(it.orderId))
-            tv1.text = it.accountId
+            tv1.text = it.orderStatus
         }
         //orderStatus
         oderStatusViewModel = ViewModelProvider(this).get(OrderStatusViewModel::class.java)
         oderStatusViewModel?.cancelOrderApi("9","752b1a44c8f596b65b98305627481261",100011)
         oderStatusViewModel?.orderStatusViewModel ?.observe(this) {
             Log.d("sddffsddsds", Gson().toJson(it.orderStatus))
-            textView.text = it.orderStatus
+           if(it.orderStatus=="4"){
+
+               textView.text = "Your order placed at 2020-11-10 00:12:06, usally Fidoo Merchant take 10 min to accept order"
+           }
 
         }
 
+
+
+        blink_anim = AnimationUtils.loadAnimation(getApplicationContext(),
+            R.anim.blink);
+        textView.startAnimation(blink_anim);
+        tv1.startAnimation(blink_anim);
+
     }
+
+
 }
