111826b69654af840948bae7e434faa13eb6ec0d
Sumit Rai <developer2607@gmail.com>
2022-03-04 11:32:43 +0530

    some bug fix and time updated

--- app/src/main/java/com/example/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -6,9 +6,13 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.Models.cancelStatus.orderStatusMSg.OrderStatusMsg
 import com.example.myapplication.Models.chatbotButtonLogic.ButtonLogicModel
+import com.example.myapplication.Models.orderStatus.OrderStatusResponse
 import com.example.myapplication.R
 import com.example.myapplication.adapter.MainAdapter
+import com.example.myapplication.adapter.botlogocount
+import com.example.myapplication.adapter.orderStatusMsgAdapter.botlogoCountforreply
 import com.example.myapplication.adapter.orderStatusMsgAdapter.orderStatusAdapter
 import com.example.myapplication.viewmodel.BotmsgViewModel
 import com.example.myapplication.viewmodel.ButtonLogicViewModel
@@ -16,6 +20,9 @@ import com.example.myapplication.viewmodel.ChatbotViewModel
 import com.example.myapplication.viewmodel.OrderStatusViewModel
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -32,13 +39,9 @@ class MainActivity :AppCompatActivity() {
     lateinit var simpleDateFormat1: SimpleDateFormat
     lateinit var mainAdapter: MainAdapter
     lateinit var orderStatusAdapter: orderStatusAdapter
-
-
-
     var newsdata=ArrayList<String>()
     var botMsgdata = ArrayList<String>()
-    var btnTextLogic = ArrayList<ButtonLogicModel>()
-
+    //var btnTextLogic = ArrayList<ButtonLogicModel>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,18 +57,26 @@ class MainActivity :AppCompatActivity() {
         simpleDateFormat1 = SimpleDateFormat(" KK:mm aaa")
         dateTime = simpleDateFormat1.format(calendar.time).toString()
         DateandTime2.text = dateTime
-       //  time1.text = dateTime
+        //  time1.text = dateTime
 //        time2.text = dateTime
 //        time3.text = dateTime
 //        time4.text = dateTime
         oderStatusViewModel = ViewModelProvider(this).get(OrderStatusViewModel::class.java)
-        oderStatusViewModel?.cancelOrderApi ("9", "752b1a44c8f596b65b98305627481261", 103550)
+        oderStatusViewModel?.cancelOrderApi("9", "752b1a44c8f596b65b98305627481261", 103550)
         oderStatusViewModel?.orderStatusViewModel?.observe(this) {
             // Log.d("sddffsddsds", Gson().toJson(it))
-            newsdata = it as ArrayList<String>
+            newsdata = it.messages as ArrayList<String>
             Log.d("sddffsddskgjgjds", Gson().toJson(it))
             setadapter()
+
+            if (it.orderStatus != "1") {
+                orderstatus.text = "Where is my order?"
+                orderconfirmStatus.text = "Where is my order?"
+            }
+
         }
+
+
 
 
 
@@ -73,24 +84,24 @@ class MainActivity :AppCompatActivity() {
 
 // main button Logic -----------------------------------------------------------------------------------------
 
-//        buttonLogicViewModel = ViewModelProvider(this).get(ButtonLogicViewModel::class.java)
-//        buttonLogicViewModel?.botmsgapiButtonLogic("10", "1a9eddfe5e5b2c75f6c15e739e05ab46", 100002)
-//        buttonLogicViewModel?.btnLogicViewModel?.observe(this) {
-//            // Log.d("sddffsddsds", Gson().toJson(it))
-//            if(it.orderStatus!="1"){
-//                orderstatus.text = "Where is my order?"
-//                orderstatus.setOnClickListener{
-//                    orderconfirmStatus.text = "Where is my order?"
-//                }
-//
-//
-//            }
+////        buttonLogicViewModel = ViewModelProvider(this).get(ButtonLogicViewModel::class.java)
+////        buttonLogicViewModel?.botmsgapiButtonLogic("10", "1a9eddfe5e5b2c75f6c15e739e05ab46", 100002)
+////        buttonLogicViewModel?.btnLogicViewModel?.observe(this) {
+////            // Log.d("sddffsddsds", Gson().toJson(it))
+////            if(it.orderStatus!="1"){
+////                orderstatus.text = "Where is my order?"
+////                orderstatus.setOnClickListener{
+////                    orderconfirmStatus.text = "Where is my order?"
+////                }
+////
+////
+////            }
 //
 //        }
-        ///////////////////////////////////////////////
+        /////////////////////////////////////////////
 
         orderstatus.setOnClickListener{
-            orderconfirmStatus.text = "Order not confirm yet"
+           // orderconfirmStatus.text = "Order not confirm yet"
             cancel.visibility = View.GONE
             orderstatus.visibility = View.GONE
             l2.visibility = View.VISIBLE
@@ -145,7 +156,6 @@ class MainActivity :AppCompatActivity() {
                 // Log.d("sddffsddsds", Gson().toJson(it))
                 botMsgdata =it as ArrayList<String>
                 Log.d("sddffsddsds", Gson().toJson(it))
-
                 setRecyclerViewBotmsg()
 
             }
@@ -179,7 +189,20 @@ class MainActivity :AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+       // botlogoCountforreply.botcount1 = 0
 
+    }
+
+    override fun onStop() {
+        super.onStop()
+        botlogoCountforreply.botcount1 = 0
+        botlogocount.botcount  = 0
+    }
+
+    override fun onPause() {
+        super.onPause()
+        botlogoCountforreply.botcount1 = 0
+        botlogocount.botcount = 0
     }
 }
 
--- app/src/main/java/com/example/myapplication/viewmodel/OrderStatusViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/OrderStatusViewModel.kt
@@ -6,43 +6,50 @@ import androidx.lifecycle.MutableLiveData
 import com.example.myapplication.Models.orderStatus.OrderStatusResponse
 import com.example.myapplication.api_request_retrofit.BackEndApi
 import com.example.myapplication.api_request_retrofit.WebServiceClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class OrderStatusViewModel (application: Application) : AndroidViewModel(application) {
 
-    var orderStatusViewModel: MutableLiveData<List<String>>? = null
+    var orderStatusViewModel: MutableLiveData<OrderStatusResponse>? = null
     var failureResponse: MutableLiveData<String>? = null
 
     init {
-        orderStatusViewModel =  MutableLiveData<List<String>>()
+        orderStatusViewModel =  MutableLiveData<OrderStatusResponse>()
         failureResponse =  MutableLiveData<String>()
 
     }
 
 
     fun cancelOrderApi(accountId: String, accessToken: String, orderId: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            WebServiceClient.client.create(BackEndApi::class.java).oredrStatusApi(
+                accountId = accountId,
+                accessToken = accessToken,
+                orderId = orderId
 
-        WebServiceClient.client.create(BackEndApi::class.java).oredrStatusApi(
-            accountId = accountId,
-            accessToken = accessToken,
-            orderId = orderId
+            )
+                .enqueue(object : Callback<OrderStatusResponse> {
 
-        )
-            .enqueue(object : Callback<OrderStatusResponse> {
-
-                override fun onResponse(call: Call<OrderStatusResponse>, response: Response<OrderStatusResponse>) {
-                    orderStatusViewModel?.value = response.body()?.messages
+                    override fun onResponse(
+                        call: Call<OrderStatusResponse>,
+                        response: Response<OrderStatusResponse>
+                    ) {
+                        orderStatusViewModel?.value = response.body()
 
 
-                }
+                    }
 
-                override fun onFailure(call: Call<OrderStatusResponse>, t: Throwable) {
+                    override fun onFailure(call: Call<OrderStatusResponse>, t: Throwable) {
 
-                    failureResponse?.value="Something went wrong"
-                }
-            })
+                        failureResponse?.value = "Something went wrong"
+                    }
+                })
+        }
     }
 
 }
